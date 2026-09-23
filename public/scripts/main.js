59c95c965c5e6944cd03c7f4a7716cc7051e320b
rrrrbit <jon.macagba.main@gmail.com>
2026-09-23 12:47:13 +0100

    wth

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -4,6 +4,8 @@ let lastScrollY = window.scrollY || window.pageYOffset;
 let internalX = lastScrollX;
 let internalY = lastScrollY;
 
+let suppressScrollEvent = false;
+
 function willOverlap(htbx, wall, targetX, targetY) {
     const wallRect = wall.getBoundingClientRect();
     const htbxRect = htbx.getBoundingClientRect();
@@ -76,90 +78,98 @@ function getProjOverlap(a0, a1, b0, b1)
     );
 }
 
-function handleOverlap() {
-    //need to make it win over scrollbar and middleclick
+function handleOverlap(currentScrollX, currentScrollY, scrollDeltaX, scrollDeltaY) {
+    // console.log("d: "+scrollDeltaX.toString()+", "+scrollDeltaY.toString()) // see scroll delta
 
-    const currentScrollX = window.scrollX || window.pageXOffset;
-    const currentScrollY = window.scrollY || window.pageYOffset;
-    
-    const scrollDeltaX = currentScrollX - lastScrollX;
-    const scrollDeltaY = currentScrollY - lastScrollY;
+    const scrollHitbox = document.getElementById('scrollHitbox');
 
-    lastScrollX = currentScrollX;
-    lastScrollY = currentScrollY;
-    //console.log("d: "+scrollDeltaX.toString()+", "+scrollDeltaY.toString()) // see scroll delta
+    internalX += scrollDeltaX;
+    internalY += scrollDeltaY;
+    //console.log("internal: "+internalX.toString()+", "+internalY.toString()) // see internal position
 
-    if(scrollDeltaX !== 0 || scrollDeltaY !== 0) {
+    // substepped collision checksssss
 
-        const scrollHitbox = document.querySelector('.scrollHitbox');
-        const testWall = document.getElementById('testWall');
+    const distance = Math.hypot(scrollDeltaX, scrollDeltaY);
 
-        if(scrollHitbox && testWall) {
-            internalX += scrollDeltaX;
-            internalY += scrollDeltaY;
-            //console.log("internal: "+internalX.toString()+", "+internalY.toString()) // see internal position
+    const step = 1;
+    const steps = Math.ceil(distance / step);
 
-            // substepped collision checksssss
+    const stepX = scrollDeltaX / steps;
+    const stepY = scrollDeltaY / steps;
 
-            const distance = Math.hypot(scrollDeltaX, scrollDeltaY);
+    let predictX = internalX - scrollDeltaX;
+    let predictY = internalY - scrollDeltaY;
+    let breakLoop = false;
+    for(let i = 0; i < steps; i++) {
+        predictX += stepX;
+        predictY += stepY;
 
-            const step = 6;
-            const steps = Math.ceil(distance / step);
+        for(const wall of walls) {
+            if(willOverlap(scrollHitbox, wall, predictX, predictY)) {
+                console.log("Overlap detected!");
 
-            const stepX = scrollDeltaX / steps;
-            const stepY = scrollDeltaY / steps;
+                const mtv = getMinimumTranslationVector(scrollHitbox, wall, predictX, predictY);
 
-            let predictX = internalX - scrollDeltaX;
-            let predictY = internalY - scrollDeltaY;
-            let breakLoop = false;
-            for(let i = 0; i < steps; i++) {
-                predictX += stepX;
-                predictY += stepY;
+                predictX += mtv.x;
+                predictY += mtv.y;
 
-                for(const wall of walls) {
-                    if(willOverlap(scrollHitbox, wall, predictX, predictY)) {
-                        console.log("Overlap detected!");
-
-                        const mtv = getMinimumTranslationVector(scrollHitbox, wall, predictX, predictY);
-
-                        predictX += mtv.x;
-                        predictY += mtv.y;
-
-                        internalX = predictX;
-                        internalY = predictY;
-                        breakLoop = true;
-                    }
-                }
-                if(breakLoop){
-                    break;
-                }
-            }
-
-            if (currentScrollX !== Math.round(internalX) || currentScrollY !== Math.round(internalY)) {
-                window.scrollTo(internalX, internalY);
-                lastScrollX = window.scrollX || window.pageXOffset;
-                lastScrollY = window.scrollY || window.pageYOffset;
+                internalX = predictX;
+                internalY = predictY;
+                breakLoop = true;
             }
         }
+        if(breakLoop){
+            break;
+        }
     }
+
+    if (currentScrollX !== Math.round(internalX) || currentScrollY !== Math.round(internalY)) {
+        hardScrollTo(internalX, internalY);
+    }
+}
+
+function hardScrollTo(x, y) {
+    // suppressScrollEvent = true;
+
+    window.scrollTo({
+        left: x,
+        top: y,
+        behavior: "instant"
+    });
+
+
+    lastScrollX = window.scrollX || window.pageXOffset;
+    lastScrollY = window.scrollY || window.pageYOffset;
+}
+
+function hardScrollIntoView(element, args) {
+    suppressScrollEvent = true;
+
+    element.scrollIntoView(args);
+
+    lastScrollX = window.scrollX || window.pageXOffset;
+    lastScrollY = window.scrollY || window.pageYOffset;
+
+    internalX = lastScrollX;
+    internalY = lastScrollY;
 }
 
 let walls = document.querySelectorAll('.wall');
 
-
+function goHome() {
+    hardScrollIntoView(document.getElementById('worldCenter'), {behavior: "instant", block: "center", inline: "center"});
+}
 
 function init() {
     walls = document.querySelectorAll('.wall');
 
-    document.querySelector('.worldCenter').scrollIntoView({ behavior: 'instant', block: 'center', inline: 'center' });
-    lastScrollX = window.scrollX || window.pageXOffset;
-    lastScrollY = window.scrollY || window.pageYOffset;
+    goHome();
     internalX = lastScrollX;
     internalY = lastScrollY;
 }
 
 function update() {
-    handleOverlap()
+    
     document.getElementById("position").textContent = window.pageXOffset.toString() + ", " + window.pageYOffset.toString();
     
     window.requestAnimationFrame(update);
@@ -174,4 +184,44 @@ window.addEventListener('mousemove', (e) => {
     document.getElementById("mouseLabel").style.top = e.pageY + "px";
     document.getElementById("mouseLabel").style.left = e.pageX + "px";
     document.getElementById("mouseLabel").textContent = e.pageX.toString() + ", " + e.pageY.toString();
-})
+});
+
+
+ 
+let ticking = false;
+let scrollIdleTimer;
+window.addEventListener('scroll', (e) => {
+
+    const suppressThisScrollEvent = suppressScrollEvent;
+    suppressScrollEvent = false;
+    if(suppressThisScrollEvent) {
+        return;
+    }
+
+    clearTimeout(scrollIdleTimer);
+
+    let currentScrollX = window.scrollX || window.pageXOffset;
+    let currentScrollY = window.scrollY || window.pageYOffset;
+
+    const scrollDeltaX = currentScrollX - lastScrollX;
+    const scrollDeltaY = currentScrollY - lastScrollY;
+
+    handleOverlap(currentScrollX, currentScrollY, scrollDeltaX, scrollDeltaY);
+    lastScrollX = currentScrollX;
+    lastScrollY = currentScrollY;
+
+    scrollIdleTimer = setTimeout(() => {
+        lastScrollX = window.scrollX || window.pageXOffset;
+        lastScrollY = window.scrollY || window.pageYOffset;
+    }, 0);
+});
+
+window.addEventListener('resize', (e) => {
+    let currentScrollX = window.scrollX || window.pageXOffset;
+    let currentScrollY = window.scrollY || window.pageYOffset;
+
+    const scrollDeltaX = currentScrollX - lastScrollX;
+    const scrollDeltaY = currentScrollY - lastScrollY;
+
+    hardScrollTo(currentScrollX - scrollDeltaX, currentScrollY - scrollDeltaY);
+});
